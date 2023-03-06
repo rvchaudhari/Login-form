11f753ff8eb0635d7d77b9ac36a2687a23aa178e
RV chaudhari <116835106+rvchaudhari@users.noreply.github.com>
2023-03-06 22:23:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+
+function myfunction()
+            {
+                var un = document.forms["myform"]["uname"].value;
+                var pw = document.forms["myform"]["pass"].value;
+                if(un=="Admin" && pw=="1234")
+                {
+                    window.location.href="Welcome/index.html";
+                }
+                else
+                {
+                    alert("invalid username and password");
+                }
+
+            }    
+            let header = document.querySelector("header");
+            let menu = document.querySelector("#menu-icon");
+            let navbar = document.querySelector(".navbar");
+
+            let darkmode = document.querySelector("#darkmode");
+            darkmode.onclick = () => {
+              if (darkmode.classList.contains("bx-moon")) {
+                darkmode.classList.replace("bx-moon", "bx-sun");
+                document.body.classList.add("active");
+              } else {
+                darkmode.classList.replace("bx-sun", "bx-moon");
+                document.body.classList.remove("active");
+              }
+            };
+
+            
+       
+
